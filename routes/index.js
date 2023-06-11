401f595d5bcbde42b4c340ce9adb66be70467ae1
ragsanil <ragsanil456@gmail.com>
2023-06-11 08:46:29 +0530

    generate

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,7 @@ let isSignedIn = false;
 let loading_message = "Loading ...⌛ We wil message you once this is done.";
 let format_not_correct_message = "Format not correct. Please try again.";
 let table_name = "";
+let schema = [];
 /* GET home page. */
 let privateApp = {
     getReply: async (msg) => {
@@ -31,13 +32,159 @@ let privateApp = {
                 table_name = matches[1];
                 console.log("SELECT table_name, column_name, data_type FROM information_schema.columns WHERE table_name = '"+table_name+"';");
                 let tableSchema = await pg.query("SELECT table_name, column_name, data_type FROM information_schema.columns WHERE table_name = '"+table_name+"';");
-                console.log(tableSchema.rows);
+                // console.log(tableSchema.rows);
                 // msg.reply("Your table name is set to: " + table_name); // Output: "auth"
+                schema = [];
+                for (const element of tableSchema.rows) {
+                    schema.push({name:element['column_name'],type:element['data_type']});
+                }
             } else {
                 await msg.reply(format_not_correct_message);
             }
             await msg.reply(loading_message);
+        } else if (msg.body.startsWith('/generate')){
+            const matches = msg.body.match(/\/generate\s+(\w+)/i);
+            let message = "";
+            if (matches){
+                message = matches[1];
+                let generate_data = await privateApp.generate(message);
+                await privateApp.addPath(generate_data);
+                // msg.reply(generate_data);
+            }
         } else await msg.reply('I\'m sorry, it seems my circuits are feeling a little under the weather today, and I\'m having trouble comprehending your message. Could you please rephrase or provide more context? I\'ll do my best to assist you once I recover from this momentary glitch. Thank you for your understanding! 🤖🤕');
+    },
+    addPath: async (components) => {
+        let res = await joeygql.admin_client(`mutation add($path: String, $data: String) {
+            insert_mia_admin_one(object: {path: $path, data: $data},on_conflict:{constraint:mia_admin_path_key,update_columns:[path,data]}) {
+              id
+            }
+          }`,{
+            path: table_name,
+            data: "components: "+JSON.stringify(components.components)
+          });
+        console.log(res);
+    },
+    generate: async(msg) => {
+        let message = msg.body;
+        console.log(schema);
+        if(schema.length==0){
+            console.log({message: `NO schema provided`});
+            return {message: `NO schema provided`};
+        }
+    
+        for (const element of schema) {
+            if (element.type === 'text') {
+                element.type = 'String';
+            }
+        }
+    
+        const schemaTypeMap = schema.reduce((result, item) => {
+            result[item.name] = item.type;
+            return result;
+        }, {});
+    
+        let columns = schema.map(e => e.name);
+    
+        let schemaString = '';
+        schema.forEach(element => {
+            schemaString += `${element.name}, ${element.type}\n`;
+        });
+    
+        let options = {
+            method: 'POST', url: 'https://api.openai.com/v1/chat/completions', headers: {
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer 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'
+            }, data: {
+                model: 'gpt-3.5-turbo', messages: [{
+                    role: 'user', content: `table schema:
+          ${schemaString}
+          
+          
+          columns:
+          ${message}
+          
+          --------
+          
+          put this int he following JSON structure
+          [
+          {
+            key: "name",
+            name: "Name",
+            type: "String",
+            showInList: true,
+            canEdit: true,
+            networkSearch: true,
+            networkSort: true,
+            save: false
+          },{
+            key: "phone",
+            name: "Phone",
+            type: "String",
+            canEdit: true,
+            showInList: true,
+            networkSearch: true,
+            networkSort: true,
+            save: false
+          }
+          ]`
+                }], temperature: 0.7
+            }
+        };
+        let response = await axios.request(options)
+        console.log(response);
+        var result = JSON.parse(response.data.choices[0].message.content);
+    
+        let headers = [];
+        let columnsToSave = [];
+        for (const element of result) {
+            if (!columns.includes(element['key'])) {
+                console.log({message: `Invalid Column : ${element['key']}`});
+                return {message: `Invalid Column : ${element['key']}`};
+            }
+            let save = element['save'];
+            if (save) {
+                columnsToSave.push(element['key']);
+            }
+            if (save !== undefined) {
+                delete element.save;
+            }
+            headers.push(element)
+        }
+    
+        let mutation = "";
+        if (columnsToSave.length > 0) {
+            mutation += `mutation updateUser($id: uuid,`;
+            for (let i = 0; i < columnsToSave.length; i++) {
+                mutation += `$${columnsToSave[i]}: ${schemaTypeMap[columnsToSave[i]]}`;
+                if (i != columnsToSave.length - 1) {
+                    mutation += ", ";
+                }
+            }
+            mutation += `) {
+          update_${table_name}(_set: {`
+            for (let i = 0; i < columnsToSave.length; i++) {
+                mutation += `${columnsToSave[i]}: $${columnsToSave[i]}`;
+                if (i != columnsToSave.length - 1) {
+                    mutation += ", ";
+                }
+            }
+            mutation += `}, where: {id: {_eq: $id}}) {
+              affected_rows
+            }
+          }`
+        }
+    
+        var components = [{
+            type: "table", id: "usersTable", title: "Users", query_item: table_name, query: `query getUsers {
+          ${table_name}(order_by: {}, where: {}, limit: 100, offset: 0) {
+            ${result.map(element => `\n    ${element.key}`).join('')}
+          }
+        }`, heads: headers, saveActions: columnsToSave.length > 0 ? [{
+                keys: columnsToSave, mutation: mutation, mutation_item: `update_${table_name}`
+            }] : []
+        }]
+    
+        return {components: components};
     }
 }
 // client = new Client({
@@ -156,9 +303,9 @@ router.post("/message", async function (req, res, next) {
     let options = {
         method: 'POST', url: 'https://api.openai.com/v1/chat/completions', headers: {
             'Content-Type': 'application/json',
-            Authorization: 'Bearer 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'
+            Authorization: 'Bearer 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'
         }, data: {
-            model: 'gpt-4', messages: [{
+            model: 'gpt-3.5-turbo', messages: [{
                 role: 'user', content: `table schema:
       ${schemaString}
       
